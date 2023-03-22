1c40777e3f1f3ddfdd33ede6ed0d9f8590a81f79
bean <wubin281@163.com>
2023-03-22 20:07:10 +0800

    update

---
+++ src/main/java/com/binwu/remoting/dto/RpcRequest.java
@@ -0,0 +1,15 @@
+package com.binwu.remoting.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class RpcRequest implements Serializable {
+    
+}
